3510d196281dec05fd169c6645f4afaef0f0b390
TSShrol <shrolts@gmail.com>
2024-09-04 19:08:30 +0300

    Added ShopTexhDBContext without using Service in Programs.cs

--- ShopTechMVC_PV321/Controllers/ProductController.cs
+++ ShopTechMVC_PV321/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTechMVC_PV321.Data;
 using ShopTechMVC_PV321.Helpers;
 using ShopTechMVC_PV321.Models;
 
@@ -7,30 +8,52 @@ namespace ShopTechMVC_PV321.Controllers
     public class ProductController : Controller
     {
         private readonly List<Product> _products;
+        private readonly ShopTechMVCDbContext _context = new ShopTechMVCDbContext();
+
         public ProductController()
         {
-            _products = SeedData.GetProduct();
+            //_products = SeedData.GetProduct();
         }
         public IActionResult Index()
         {
             //TODO: dbcontext
-            return View(_products);
+            //return View(_products);
+            return View(_context.Products.ToList<Product>());
         }
-        
+
+        //public IActionResult Details(int id)
+        //{
+        //    //var id =int.Parse(HttpContext.Request.Query["id"]);
+        //    var product = _products.FirstOrDefault(p => p.Id == id);
+        //    return View(product);
+
+        //}
+        //[Route("Product/Details2/{id:int}")]
+        [Route("Product/Details/{id:int}")]
         public IActionResult Details(int id)
         {
-            //var id =int.Parse(HttpContext.Request.Query["id"]);
-            var product = _products.FirstOrDefault(p => p.Id == id);
+            //https://localhost:7001/Product/Details?id=1
+            // var id = int.Parse(Request.Query["id"]);
+            //https://localhost:7001/Product/Details/1
+            // var id = int.Parse(RouteData.Values["id"].ToString());
+
+            //var product = _products.FirstOrDefault(p => p.Id == id);
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
             return View(product);
 
         }
         public IActionResult Delete(int id) { 
             //find in database
-            var product = _products.FirstOrDefault(product => product.Id == id);
+            //var product = _products.FirstOrDefault(product => product.Id == id);
+            var product = _context.Products.FirstOrDefault(product => product.Id == id);
             if (product != null) {
-                _products.Remove(product);
+                //_products.Remove(product);
+                _context.Products.Remove(product);
+                _context.SaveChanges();
             }
-            return View("Index",_products);
+         
+            //return View("Index",_products);
+            return View("Index",_context.Products.ToList<Product>());
         }
     }
 }
---
+++ ShopTechMVC_PV321/Data/ShopTechMVCDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTechMVC_PV321.Helpers;
+using ShopTechMVC_PV321.Models;
+namespace ShopTechMVC_PV321.Data
+{
+    public class ShopTechMVCDbContext:DbContext
+    {
+        public ShopTechMVCDbContext()
+        {
+            //Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ShopTechMVC;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().HasData(SeedData.GetProduct());
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
